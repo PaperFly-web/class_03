24444a6eafe3eaced93c5525d7117b024dd51803
PaperFly-web <62205559+PaperFly-web@users.noreply.github.com>
2020-12-26 14:36:46 +0800

    Update NetworkScheduling.java

--- main/java/com/paperfly/campusNetworkUtils/schedule/NetworkScheduling.java
+++ main/java/com/paperfly/campusNetworkUtils/schedule/NetworkScheduling.java
@@ -72,7 +72,7 @@ public class NetworkScheduling {
 
 
         // 请求体，包括请求数据 body 和 请求头 headers
-        HttpEntity httpEntity = new HttpEntity("userId=092818106&password=PaperFly916924&service=%25E7%25A7%25BB%25E5%258A%25A8&queryString=wlanuserip%253D419f4ad9b1c0e383a75f70f312014cc1%2526wlanacname%253Dfdce1bc241f634bf1bb1c06a02479fe6%2526ssid%253D%2526nasip%253D30694bc990ef8fb530d9b634c75f78f0%2526snmpagentip%253D%2526mac%253Dcfb1ad60573dd740f2c77c30e96a1ab2%2526t%253Dwireless-v2%2526url%253D63dd0e45e34314b31f5fe54250d8b3ba0328c9bbafeb9fbf7081d7b4785a729d%2526apmac%253D%2526nasid%253Dfdce1bc241f634bf1bb1c06a02479fe6%2526vid%253D78263ede2c6c1f01%2526port%253D7e202b12eef557c0%2526nasportid%253Dc5e1bd3f32f8f6ac8e41a216cd1c8e2d44a43f6bde48dd3386f8ba0f5ef950dc9ebc76cfb8aee516&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false",headers);
+        HttpEntity httpEntity = new HttpEntity("userId=xxxxxxx&password=xxxxxx&service=%25E7%25A7%25BB%25E5%258A%25A8&queryString=wlanuserip%253D419f4ad9b1c0e383a75f70f312014cc1%2526wlanacname%253Dfdce1bc241f634bf1bb1c06a02479fe6%2526ssid%253D%2526nasip%253D30694bc990ef8fb530d9b634c75f78f0%2526snmpagentip%253D%2526mac%253Dcfb1ad60573dd740f2c77c30e96a1ab2%2526t%253Dwireless-v2%2526url%253D63dd0e45e34314b31f5fe54250d8b3ba0328c9bbafeb9fbf7081d7b4785a729d%2526apmac%253D%2526nasid%253Dfdce1bc241f634bf1bb1c06a02479fe6%2526vid%253D78263ede2c6c1f01%2526port%253D7e202b12eef557c0%2526nasportid%253Dc5e1bd3f32f8f6ac8e41a216cd1c8e2d44a43f6bde48dd3386f8ba0f5ef950dc9ebc76cfb8aee516&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false",headers);
 
         try {
             //使用 exchange 发送请求，以String的类型接收返回的数据
